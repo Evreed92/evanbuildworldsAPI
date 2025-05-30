a4eb5ba63686cfccbea80a4b0e728d1ed7973dce
EVAN REED <er0997669@otc.edu>
2025-05-30 00:04:45 -0500

    Connecting to db process started. Working on context and table schemas

--- MyDbContext.cs
+++ MyDbContext.cs
@@ -4,6 +4,7 @@ namespace evanbuildsworldsAPI
 {
     public class MyDbContext : DbContext
     {
-
+        //Need to add table schema? Reserach details
+        //need to add connection string
     }
 }
