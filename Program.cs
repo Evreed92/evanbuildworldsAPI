a7a33311f3ec7e3782cbc80f70631968b033f556
EVAN REED <er0997669@otc.edu>
2025-06-02 23:03:14 -0500

    Add CORS for localhost connection to Next.js frontend

--- Program.cs
+++ Program.cs
@@ -13,6 +13,17 @@ namespace evanbuildsworldsAPI
 
             var builder = WebApplication.CreateBuilder(args);
 
+            //Adding CORS for localhosting
+            builder.Services.AddCors(options =>
+                        {
+                            options.AddPolicy("AllowFrontend",
+                                policy =>
+                                {
+                                    policy.WithOrigins("http://localhost:3000")
+                                        .AllowAnyMethod()
+                                        .AllowAnyHeader();
+                                });
+                        });
             //database config
             builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Add services to the container.
@@ -20,11 +31,11 @@ namespace evanbuildsworldsAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            
-
-
 
             var app = builder.Build();
+
+            //Add CORS Policy
+            app.UseCors("AllowFrontend");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
